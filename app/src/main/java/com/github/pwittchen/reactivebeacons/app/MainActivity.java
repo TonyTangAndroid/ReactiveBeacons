298c421de2ae0883e14e799c43a99e30433c947d
ztang <tonythompsoncmu@gmail.com>
2018-08-04 12:35:53 -0400

    format code

--- app/src/main/java/com/github/pwittchen/reactivebeacons/app/MainActivity.java
+++ app/src/main/java/com/github/pwittchen/reactivebeacons/app/MainActivity.java
@@ -24,148 +24,156 @@ import android.support.v4.app.ActivityCompat;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
 import com.github.pwittchen.reactivebeacons.R;
 import com.github.pwittchen.reactivebeacons.library.rx2.Beacon;
 import com.github.pwittchen.reactivebeacons.library.rx2.Proximity;
 import com.github.pwittchen.reactivebeacons.library.rx2.ReactiveBeacons;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
 
 public class MainActivity extends Activity {
-  private static final boolean IS_AT_LEAST_ANDROID_M =
-      Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
-  private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1000;
-  private static final String ITEM_FORMAT = "MAC: %s, RSSI: %d\ndistance: %.2fm, proximity: %s\n%s";
-  private ReactiveBeacons reactiveBeacons;
-  private Disposable subscription;
-  private ListView lvBeacons;
-  private Map<String, Beacon> beacons;
+    private static final boolean IS_AT_LEAST_ANDROID_M =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1000;
+    private static final String ITEM_FORMAT = "MAC: %s, RSSI: %d\ndistance: %.2fm, proximity: %s\n%s";
+    private ReactiveBeacons reactiveBeacons;
+    private Disposable subscription;
+    private ListView lvBeacons;
+    private Map<String, Beacon> beacons;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    lvBeacons = (ListView) findViewById(R.id.lv_beacons);
-    reactiveBeacons = new ReactiveBeacons(this);
-    beacons = new HashMap<>();
-  }
-
-  @Override protected void onResume() {
-    super.onResume();
-
-    if (!canObserveBeacons()) {
-      return;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvBeacons = (ListView) findViewById(R.id.lv_beacons);
+        reactiveBeacons = new ReactiveBeacons(this);
+        beacons = new HashMap<>();
     }
 
-    startSubscription();
-  }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-  private void startSubscription() {
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-        != PackageManager.PERMISSION_GRANTED
-        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-        != PackageManager.PERMISSION_GRANTED) {
-      requestCoarseLocationPermission();
-      return;
+        if (!canObserveBeacons()) {
+            return;
+        }
+
+        startSubscription();
     }
 
-    subscription = reactiveBeacons.observe()
-        .subscribeOn(Schedulers.computation())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Consumer<Beacon>() {
-          @Override public void accept(@NonNull Beacon beacon) throws Exception {
-            beacons.put(beacon.device.getAddress(), beacon);
-            refreshBeaconList();
-          }
-        });
-  }
+    private void startSubscription() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestCoarseLocationPermission();
+            return;
+        }
 
-  private boolean canObserveBeacons() {
-    if (!reactiveBeacons.isBleSupported()) {
-      Toast.makeText(this, "BLE is not supported on this device", Toast.LENGTH_SHORT).show();
-      return false;
+        subscription = reactiveBeacons.observe()
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Beacon>() {
+                    @Override
+                    public void accept(@NonNull Beacon beacon) throws Exception {
+                        beacons.put(beacon.device.getAddress(), beacon);
+                        refreshBeaconList();
+                    }
+                });
     }
 
-    if (!reactiveBeacons.isBluetoothEnabled()) {
-      reactiveBeacons.requestBluetoothAccess(this);
-      return false;
-    } else if (!reactiveBeacons.isLocationEnabled(this)) {
-      reactiveBeacons.requestLocationAccess(this);
-      return false;
-    } else if (!isFineOrCoarseLocationPermissionGranted() && IS_AT_LEAST_ANDROID_M) {
-      requestCoarseLocationPermission();
-      return false;
+    private boolean canObserveBeacons() {
+        if (!reactiveBeacons.isBleSupported()) {
+            Toast.makeText(this, "BLE is not supported on this device", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!reactiveBeacons.isBluetoothEnabled()) {
+            reactiveBeacons.requestBluetoothAccess(this);
+            return false;
+        } else if (!reactiveBeacons.isLocationEnabled(this)) {
+            reactiveBeacons.requestLocationAccess(this);
+            return false;
+        } else if (!isFineOrCoarseLocationPermissionGranted() && IS_AT_LEAST_ANDROID_M) {
+            requestCoarseLocationPermission();
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    private void refreshBeaconList() {
+        List<String> list = new ArrayList<>();
 
-  private void refreshBeaconList() {
-    List<String> list = new ArrayList<>();
+        for (Beacon beacon : beacons.values()) {
+            list.add(getBeaconItemString(beacon));
+        }
 
-    for (Beacon beacon : beacons.values()) {
-      list.add(getBeaconItemString(beacon));
+        int itemLayoutId = android.R.layout.simple_list_item_1;
+        lvBeacons.setAdapter(new ArrayAdapter<>(this, itemLayoutId, list));
     }
 
-    int itemLayoutId = android.R.layout.simple_list_item_1;
-    lvBeacons.setAdapter(new ArrayAdapter<>(this, itemLayoutId, list));
-  }
-
-  private String getBeaconItemString(Beacon beacon) {
-    String mac = beacon.device.getAddress();
-    int rssi = beacon.rssi;
-    double distance = beacon.getDistance();
-    Proximity proximity = beacon.getProximity();
-    String name = beacon.device.getName();
-    return String.format(ITEM_FORMAT, mac, rssi, distance, proximity, name);
-  }
-
-  @Override protected void onPause() {
-    super.onPause();
-    if (subscription != null && !subscription.isDisposed()) {
-      subscription.dispose();
+    private String getBeaconItemString(Beacon beacon) {
+        String mac = beacon.device.getAddress();
+        int rssi = beacon.rssi;
+        double distance = beacon.getDistance();
+        Proximity proximity = beacon.getProximity();
+        String name = beacon.device.getName();
+        return String.format(ITEM_FORMAT, mac, rssi, distance, proximity, name);
     }
-  }
 
-  @Override public void onRequestPermissionsResult(int requestCode,
-      @android.support.annotation.NonNull String[] permissions,
-      @android.support.annotation.NonNull int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    final boolean isCoarseLocation = requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION;
-    final boolean permissionGranted = grantResults[0] == PERMISSION_GRANTED;
-
-    if (isCoarseLocation && permissionGranted && subscription == null) {
-      startSubscription();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
     }
-  }
 
-  private void requestCoarseLocationPermission() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      requestPermissions(new String[] { ACCESS_COARSE_LOCATION },
-          PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @android.support.annotation.NonNull String[] permissions,
+                                           @android.support.annotation.NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        final boolean isCoarseLocation = requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION;
+        final boolean permissionGranted = grantResults[0] == PERMISSION_GRANTED;
+
+        if (isCoarseLocation && permissionGranted && subscription == null) {
+            startSubscription();
+        }
     }
-  }
 
-  private boolean isFineOrCoarseLocationPermissionGranted() {
-    boolean isAndroidMOrHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
-    boolean isFineLocationPermissionGranted = isGranted(ACCESS_FINE_LOCATION);
-    boolean isCoarseLocationPermissionGranted = isGranted(ACCESS_COARSE_LOCATION);
+    private void requestCoarseLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+        }
+    }
 
-    return isAndroidMOrHigher && (isFineLocationPermissionGranted
-        || isCoarseLocationPermissionGranted);
-  }
+    private boolean isFineOrCoarseLocationPermissionGranted() {
+        boolean isAndroidMOrHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+        boolean isFineLocationPermissionGranted = isGranted(ACCESS_FINE_LOCATION);
+        boolean isCoarseLocationPermissionGranted = isGranted(ACCESS_COARSE_LOCATION);
 
-  private boolean isGranted(String permission) {
-    return ActivityCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED;
-  }
+        return isAndroidMOrHigher && (isFineLocationPermissionGranted
+                || isCoarseLocationPermissionGranted);
+    }
+
+    private boolean isGranted(String permission) {
+        return ActivityCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED;
+    }
 }
