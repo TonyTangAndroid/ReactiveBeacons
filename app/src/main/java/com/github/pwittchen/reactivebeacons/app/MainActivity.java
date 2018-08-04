6d80d9dcea0a959448d1ca1b52ffaf6cd6089bbe
ztang <tonythompsoncmu@gmail.com>
2018-08-04 13:24:00 -0400

    Add recycler view with diff util

--- app/src/main/java/com/github/pwittchen/reactivebeacons/app/MainActivity.java
+++ app/src/main/java/com/github/pwittchen/reactivebeacons/app/MainActivity.java
@@ -15,15 +15,19 @@
  */
 package com.github.pwittchen.reactivebeacons.app;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.support.annotation.NonNull;
+import android.support.v4.util.Pair;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.pwittchen.reactivebeacons.R;
 import com.github.pwittchen.reactivebeacons.library.rx2.Beacon;
@@ -35,105 +39,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
+import static io.reactivex.android.schedulers.AndroidSchedulers.mainThread;
+import static io.reactivex.schedulers.Schedulers.computation;
 
 public class MainActivity extends Activity {
-    private static final boolean IS_AT_LEAST_ANDROID_M =
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
-    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1000;
+
+
     private static final String ITEM_FORMAT = "MAC: %s, RSSI: %d\ndistance: %.2fm, proximity: %s\n%s";
     private ReactiveBeacons reactiveBeacons;
     private Disposable subscription;
-    private ListView lvBeacons;
     private Map<String, Beacon> beacons;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvBeacons = (ListView) findViewById(R.id.lv_beacons);
+        initView();
         reactiveBeacons = new ReactiveBeacons(this);
         beacons = new HashMap<>();
+    }
+
+    private void initView() {
+        RecyclerView recyclerView = findViewById(R.id.rv_beacons);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter();
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        List<Beacon> emptyList = new ArrayList<>();
+        adapter.setBeacons(emptyList);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        if (!canObserveBeacons()) {
-            return;
-        }
-
         startSubscription();
-    }
-
-    private void startSubscription() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestCoarseLocationPermission();
-            return;
-        }
-
-        subscription = reactiveBeacons.observe()
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Beacon>() {
-                    @Override
-                    public void accept(@NonNull Beacon beacon) throws Exception {
-                        beacons.put(beacon.device.getAddress(), beacon);
-                        refreshBeaconList();
-                    }
-                });
-    }
-
-    private boolean canObserveBeacons() {
-        if (!reactiveBeacons.isBleSupported()) {
-            Toast.makeText(this, "BLE is not supported on this device", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-
-        if (!reactiveBeacons.isBluetoothEnabled()) {
-            reactiveBeacons.requestBluetoothAccess(this);
-            return false;
-        } else if (!reactiveBeacons.isLocationEnabled(this)) {
-            reactiveBeacons.requestLocationAccess(this);
-            return false;
-        } else if (!isFineOrCoarseLocationPermissionGranted() && IS_AT_LEAST_ANDROID_M) {
-            requestCoarseLocationPermission();
-            return false;
-        }
-
-        return true;
-    }
-
-    private void refreshBeaconList() {
-        List<String> list = new ArrayList<>();
-
-        for (Beacon beacon : beacons.values()) {
-            list.add(getBeaconItemString(beacon));
-        }
-
-        int itemLayoutId = android.R.layout.simple_list_item_1;
-        lvBeacons.setAdapter(new ArrayAdapter<>(this, itemLayoutId, list));
-    }
-
-    private String getBeaconItemString(Beacon beacon) {
-        String mac = beacon.device.getAddress();
-        int rssi = beacon.rssi;
-        double distance = beacon.getDistance();
-        Proximity proximity = beacon.getProximity();
-        String name = beacon.device.getName();
-        return String.format(ITEM_FORMAT, mac, rssi, distance, proximity, name);
     }
 
     @Override
@@ -144,36 +86,132 @@ public class MainActivity extends Activity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @android.support.annotation.NonNull String[] permissions,
-                                           @android.support.annotation.NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        final boolean isCoarseLocation = requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION;
-        final boolean permissionGranted = grantResults[0] == PERMISSION_GRANTED;
 
-        if (isCoarseLocation && permissionGranted && subscription == null) {
-            startSubscription();
+    private void startSubscription() {
+
+        subscription = reactiveBeacons.observe()
+//                .filter(this::near)
+//                .filter(this::fixedBeacon)
+                .map(this::toListResult)
+                .scan(Pair.create(new ArrayList<>(), null), this::toPair)
+                .skip(1)
+                .subscribeOn(computation())
+                .observeOn(mainThread())
+                .subscribe(this::refreshUI);
+    }
+
+    private boolean fixedBeacon(Beacon beacon) {
+        return beacon.macAddress.address.endsWith("75:EE");
+    }
+
+
+    private boolean near(Beacon beacon) {
+        return beacon.getProximity().equals(Proximity.IMMEDIATE);
+    }
+
+    private void refreshUI(Pair<List<Beacon>, DiffUtil.DiffResult> listDiffResultPair) {
+        adapter.setBeacons(listDiffResultPair.first);
+        DiffUtil.DiffResult second = listDiffResultPair.second;
+        if (second != null)
+            second.dispatchUpdatesTo(adapter);
+    }
+
+    @NonNull
+    private Pair<List<Beacon>, DiffUtil.DiffResult> toPair(Pair<List<Beacon>, DiffUtil.DiffResult> pair, List<Beacon> next) {
+        MyDiffCallback callback = new MyDiffCallback(pair.first, next);
+        DiffUtil.DiffResult result = DiffUtil.calculateDiff(callback);
+        return Pair.create(next, result);
+    }
+
+    private List<Beacon> toListResult(Beacon beacon) {
+        beacons.put(beacon.device.getAddress(), beacon);
+        return new ArrayList<>(beacons.values());
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    private static String getBeaconItemString(Beacon beacon) {
+        String mac = beacon.device.getAddress();
+        int rssi = beacon.rssi;
+        double distance = beacon.getDistance();
+        Proximity proximity = beacon.getProximity();
+        String name = beacon.device.getName();
+        return String.format(ITEM_FORMAT, mac, rssi, distance, proximity, name);
+    }
+
+
+    private static class MyAdapter extends RecyclerView.Adapter<BeaconViewHolder> {
+        private List<Beacon> things = new ArrayList<>(); // Start with empty list
+
+        @android.support.annotation.NonNull
+        @Override
+        public BeaconViewHolder onCreateViewHolder(@android.support.annotation.NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_beacon, parent, false);
+            return new BeaconViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@android.support.annotation.NonNull BeaconViewHolder holder, int position) {
+            Beacon thing = things.get(position);
+            holder.bind(thing);
+        }
+
+        @Override
+        public int getItemCount() {
+            return things.size();
+        }
+
+        public void setBeacons(List<Beacon> things) {
+            this.things = things;
         }
     }
 
-    private void requestCoarseLocationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{ACCESS_COARSE_LOCATION},
-                    PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+    private static class BeaconViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public BeaconViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text);
+        }
+
+        public void bind(Beacon thing) {
+            textView.setText(getBeaconItemString(thing));
         }
     }
 
-    private boolean isFineOrCoarseLocationPermissionGranted() {
-        boolean isAndroidMOrHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
-        boolean isFineLocationPermissionGranted = isGranted(ACCESS_FINE_LOCATION);
-        boolean isCoarseLocationPermissionGranted = isGranted(ACCESS_COARSE_LOCATION);
+    private static class MyDiffCallback extends DiffUtil.Callback {
+        private List<Beacon> current;
+        private List<Beacon> next;
 
-        return isAndroidMOrHigher && (isFineLocationPermissionGranted
-                || isCoarseLocationPermissionGranted);
-    }
+        public MyDiffCallback(List<Beacon> current, List<Beacon> next) {
+            this.current = current;
+            this.next = next;
+        }
 
-    private boolean isGranted(String permission) {
-        return ActivityCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED;
+        @Override
+        public int getOldListSize() {
+            return current.size();
+        }
+
+        @Override
+        public int getNewListSize() {
+            return next.size();
+        }
+
+        @Override
+        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            Beacon currentItem = current.get(oldItemPosition);
+            Beacon nextItem = next.get(newItemPosition);
+            return currentItem.device.getAddress().equals(nextItem.device.getAddress());
+        }
+
+        @Override
+        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+            Beacon currentItem = current.get(oldItemPosition);
+            Beacon nextItem = next.get(newItemPosition);
+            return currentItem.toString().equals(nextItem.toString());
+        }
     }
 }
